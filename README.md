fa90cd1e070fa9e3f9ccb76f409a04f07bba805d
RebooteDuck <rebooteduck@gmail.com>
2024-11-09 22:18:31 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a test for roughly simulating the arena for Robocon, probably doesn't work very well
---
+++ log.py
@@ -0,0 +1,9 @@
+# Not finished
+
+import tkinter
+class logger:
+    def __init__(self, name: str):
+        self.window = tkinter.tk()
+        self.name = name
+        print("New logging interface created")
+        
